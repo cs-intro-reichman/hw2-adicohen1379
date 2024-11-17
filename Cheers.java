8876b98337ea4ceabaa51dc8ebafcc5f309b63be
adi cohen <adi.cohen05@post.runi.ac.il>
2024-11-17 17:22:36 +0200

    Solved

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,45 @@
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word=args[0];
+                Integer num=Integer.parseInt(args[1]);
+                for(int i=0;i<args[0].length();i++)
+                {
+                        word=args[0].toUpperCase();
+                        if(word.charAt(i)==('A'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
+                        else if(word.charAt(i)==('E'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!"); 
+                        else if(word.charAt(i)==('F'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
+                        else if(word.charAt(i)==('H'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
+                        else if(word.charAt(i)==('I'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
+                        else if(word.charAt(i)==('L'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
+                        else if(word.charAt(i)==('M'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!"); 
+                        else if(word.charAt(i)==('N'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
+                        else if(word.charAt(i)==('O'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
+                        else if(word.charAt(i)==('R'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
+                        else if(word.charAt(i)==('S'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
+                        else if(word.charAt(i)==('X'))
+                                 System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
+                        else
+                                 System.out.println("Give me a "+word.charAt(i)+" :"+word.charAt(i)+"!"); 
+
+                }
+                System.out.println("What does that spell?");
+                Integer j=0;
+                while(j<num)
+                {
+                        System.out.println(word+"!!!");
+                        j++;
+                }
+                
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,76 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+        Integer n=Integer.parseInt(args[0]);
+		String mode=args[1];
+		Integer i=1;
+		Integer k=1;
+		Integer d=0;
+		Integer counter=1;
+		Integer counter2=0;
+			if(mode.equals("v"))
+			{
+				System.out.print(i+" ");
+				for(int j=0;(j<n+1)&&(counter2!=n);j++)
+				{
+					j=d;
+					if(i%2==0)
+					{
+						i=(i/2);
+						System.out.print(i+" ");
+						counter++;
+					}
+					else
+					{
+						i=(i*3)+1;
+						System.out.print(i+" ");
+						counter++;
+					}
+					if((i==1.0))
+					{
+						System.out.println("("+counter+")");
+						k++;
+						i=k;
+						counter2++;;
+						counter=1;
+						d++;
+						if(counter2!=n)
+						{
+							System.out.print(i+" ");
+						}
+						
+					}
+				}	
+				if(counter2==n)
+					{
+						System.out.println("Every one of the first "+n+" hailstone sequences reached 1.");
+					}
+			}
+			if(mode.equals("c"))
+			{
+				for(int j=0;(j<n+1)&&(counter2!=n);j++)
+				{
+					j=d;
+					if(i%2==0)
+					{
+						i=(i/2);			
+					}
+					else
+					{
+						i=(i*3)+1;
+					}
+					if((i==1.0))
+					{
+						counter2++;;
+						k++;
+						i=k;
+						d++;
+					}
+				}	
+				if(counter2==n)
+					{
+						System.out.println("Every one of the first "+n+" hailstone sequences reached 1.");
+					}
+			}
 	}
 }
