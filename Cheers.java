23ab9680b730d9236e3e6249778830bf17b5fbd8
adi cohen <adi.cohen05@post.runi.ac.il>
2024-11-17 17:52:16 +0200

    Solved

--- Cheers.java
+++ Cheers.java
@@ -30,7 +30,7 @@ public class Cheers {
                         else if(word.charAt(i)==('X'))
                                  System.out.println("Give me an "+word.charAt(i)+": "+word.charAt(i)+"!");
                         else
-                                 System.out.println("Give me a "+word.charAt(i)+": "+word.charAt(i)+"!"); 
+                                 System.out.println("Give me a  "+word.charAt(i)+": "+word.charAt(i)+"!"); 
 
                 }
                 System.out.println("What does that spell?");
